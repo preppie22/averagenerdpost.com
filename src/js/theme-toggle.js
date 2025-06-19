2fa6bb37c6dbe49e827b06cc54fa1b4ac47140d4
Piyush Puranik <piyushpuranik@gmail.com>
2025-06-19 09:43:08 -0700

    Fix jarring theme change on page load

--- src/js/theme-toggle.js
+++ src/js/theme-toggle.js
@@ -1,29 +1,19 @@
-window.addEventListener("DOMContentLoaded", (event) => {
-    var page = document.getElementsByTagName("body")[0];
-    const currentTheme = localStorage.getItem("data-theme") ? localStorage.getItem("data-theme") : null;
-    if (currentTheme == "dark") {
-        page.setAttribute('data-theme','dark');
-    } else  if (currentTheme == "light") {
-        page.setAttribute('data-theme', 'light');
-    } else {
-        if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-            page.setAttribute('data-theme', 'dark');
-            localStorage.setItem('data-theme', 'dark');
-        } else {
-            page.setAttribute('data-theme', 'light');
-            localStorage.setItem('data-theme', 'light');
-        }
-    }
-});
+function initialize_theme() {
+    let theme = localStorage.getItem('data-theme');
 
+    if (!theme) {
+        const isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+        theme = isDarkMode ? 'dark' : 'light';
+        localStorage.setItem('data-theme', theme);
+    }
+
+    document.documentElement.setAttribute('data-theme', theme);
+};
 function toggle_light_mode() {
-    var app = document.getElementsByTagName("BODY")[0];
+    const root = document.documentElement;
     const currentTheme = localStorage.getItem("data-theme");
-    if (currentTheme == "dark") {
-        localStorage.setItem("data-theme", "light");
-        app.setAttribute("data-theme", "light");
-    } else {
-        localStorage.setItem("data-theme", "dark");
-        app.setAttribute("data-theme", "dark");
-    }		
+    const newTheme = currentTheme === "dark" ? "light" : "dark";
+
+    localStorage.setItem("data-theme", newTheme);
+    root.setAttribute("data-theme", newTheme);
 }
