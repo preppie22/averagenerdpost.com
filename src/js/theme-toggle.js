bb531a31cb9b0133aaf0bf302c11e5612842b737
Piyush Puranik <piyushpuranik@gmail.com>
2025-06-19 11:33:20 -0700

    Use simpler theme toggle switch for clarity

--- src/js/theme-toggle.js
+++ src/js/theme-toggle.js
@@ -1,3 +1,14 @@
+// function updateThemeIcon(theme) {
+//     const icon = document.getElementById('light-mode-icon');
+//     if (theme === 'dark') {
+//         icon.src = '/assets/icons/dark-mode-moon.svg';
+//         icon.alt = 'Switch to light mode';
+//     } else {
+//         icon.src = '/assets/icons/light-mode-sun.svg';
+//         icon.alt = 'Switch to dark mode';
+//     }
+// }
+
 function initialize_theme() {
     let theme = localStorage.getItem('data-theme');
 
@@ -8,7 +19,9 @@ function initialize_theme() {
     }
 
     document.documentElement.setAttribute('data-theme', theme);
+    // updateThemeIcon(theme);
 };
+
 function toggle_light_mode() {
     const root = document.documentElement;
     const currentTheme = localStorage.getItem("data-theme");
@@ -16,4 +29,5 @@ function toggle_light_mode() {
 
     localStorage.setItem("data-theme", newTheme);
     root.setAttribute("data-theme", newTheme);
+    // updateThemeIcon(newTheme);
 }
